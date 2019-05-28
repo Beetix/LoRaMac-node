836efeb68f232644525a97ad704b852e92574986
Beetix <b.t.freeman16@gmail.com>
2019-05-29 00:11:17 +0200

    Add RHF76-052 module support
    - RisingHF LoRaWAN module: http://www.risinghf.com/#/product-details?product_id=5&lang=en
    - Add class A example
    - Based on NucleoL073 code

---
+++ src/boards/RHF76-052/board-config.h
@@ -0,0 +1,81 @@
+/*!
+ * \file      board-config.h
+ *
+ * \brief     Board configuration
+ *
+ * \copyright Revised BSD License, see section \ref LICENSE.
+ *
+ * \code
+ *                ______                              _
+ *               / _____)             _              | |
+ *              ( (____  _____ ____ _| |_ _____  ____| |__
+ *               \____ \| ___ |    (_   _) ___ |/ ___)  _ \
+ *               _____) ) ____| | | || |_| ____( (___| | | |
+ *              (______/|_____)_|_|_| \__)_____)\____)_| |_|
+ *              (C)2013-2017 Semtech
+ *
+ *               ___ _____ _   ___ _  _____ ___  ___  ___ ___
+ *              / __|_   _/_\ / __| |/ / __/ _ \| _ \/ __| __|
+ *              \__ \ | |/ _ \ (__| ' <| _| (_) |   / (__| _|
+ *              |___/ |_/_/ \_\___|_|\_\_| \___/|_|_\\___|___|
+ *              embedded.connectivity.solutions===============
+ *
+ * \endcode
+ *
+ * \author    Miguel Luis ( Semtech )
+ *
+ * \author    Gregory Cristian ( Semtech )
+ *
+ * \author    Daniel Jaeckle ( STACKFORCE )
+ *
+ * \author    Johannes Bruder ( STACKFORCE )
+ */
+#ifndef __BOARD_CONFIG_H__
+#define __BOARD_CONFIG_H__
+
+#define BOARD_TCXO_WAKEUP_TIME                      0
+
+/*!
+ * Board MCU pins definitions
+ */
+#define RADIO_RESET                                 PB_11
+
+#define RADIO_MOSI                                  PA_7
+#define RADIO_MISO                                  PA_6
+#define RADIO_SCLK                                  PA_5
+
+#define RADIO_NSS                                   PA_4
+
+#define RADIO_DIO_0                                 PB_10
+#define RADIO_DIO_1                                 PB_2
+#define RADIO_DIO_2                                 PB_0
+#define RADIO_DIO_3                                 PB_1
+#define RADIO_DIO_4                                 NC
+#define RADIO_DIO_5                                 NC
+
+#define RADIO_ANT_SWITCH_RX                         PA_2
+#define RADIO_ANT_SWITCH_TX                         PA_1
+
+#define LED_1                                       PA_8
+#define LED_2                                       NC
+
+#define OSC_LSE_IN                                  PC_14
+#define OSC_LSE_OUT                                 PC_15
+
+#define OSC_HSE_IN                                  PH_0
+#define OSC_HSE_OUT                                 PH_1
+
+#define SWCLK                                       PA_14
+#define SWDAT                                       PA_13
+
+#define I2C_SCL                                     PB_8
+#define I2C_SDA                                     PB_9
+
+#define UART_TX                                     PB_6
+#define UART_RX                                     PB_7
+
+// Debug pins definition.
+#define RADIO_DBG_PIN_TX                            NC
+#define RADIO_DBG_PIN_RX                            NC
+
+#endif // __BOARD_CONFIG_H__
